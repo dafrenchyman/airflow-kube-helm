3a268a9c6422d563e0055b3a20d402c2bd1e6350
Julien Pierret <julien.pierret@appfolio.com>
2019-07-24 08:30:45 -0700

    kb - skip the whole nohup file dumping

--- bin/port_forwarding.sh
+++ bin/port_forwarding.sh
@@ -12,5 +12,4 @@ rm nohup.out
 AIRFLOW_POD=`kubectl get pod --namespace ${NAMESPACE} --selector="app=airflow-web,release=airflow" --output jsonpath='{.items[0].metadata.name}'`
 AIRFLOW_PORT=8080
 
-nohup kubectl port-forward --namespace ${NAMESPACE} ${AIRFLOW_POD} ${AIRFLOW_PORT}:${AIRFLOW_PORT} &
-
+nohup kubectl port-forward --namespace ${NAMESPACE} ${AIRFLOW_POD} ${AIRFLOW_PORT}:${AIRFLOW_PORT} </dev/null >/dev/null 2>&1 &
